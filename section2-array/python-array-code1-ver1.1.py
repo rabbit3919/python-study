da3279f4d093d080921a477985c192d089c42a9a
jihye-song1 <rabbit3919@gmail.com>
2024-01-07 19:11:05 +0900

    SECTION2. 배열과 연결리스트 - 문제1_v1.1

---
+++ section2-array/python-array-code1-ver1.1.py
@@ -0,0 +1,21 @@
+# 수열의 원소에서 가장 작은 값을 찾을 때 
+# 매개변수 num에 길이가 n / 수열의 원소 중 가장 작은 값을 찾기 
+
+def solution(list):
+  #list는 배열 입력 
+  listsize=len(list) #배열 사이즈 구하기 
+  min=list[0]        #최솟값을 배열의 0번째 값으로 설정
+
+
+  
+  for i in range(1,listsize):
+    if min > list[i]:  #설정한 min 값이 배열의 값보다 크면  
+      min=list[i]      #배열의 값을 min으로 설정
+
+  print("minimun value is ",min)
+
+
+print(solution([7,10,5,2,15,19]))
+print(solution([-12,12,30,-15,-5,3,9,-11,14]))
+print(solution([17,11,5,8,23,29,19,12,25,16,-2]))
+print(solution([7,5,12,-9,-12,22,-30,-35,-21]))
