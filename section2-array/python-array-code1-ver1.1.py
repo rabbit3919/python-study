6dfb760aaf17b4850f63434c1f096d41055a79e0
jihye-song1 <rabbit3919@gmail.com>
2024-01-07 19:13:59 +0900

    SECTION2.배열과 연결리스트_문제1_v1.1

--- section2-array/python-array-code1-ver1.1.py
+++ section2-array/python-array-code1-ver1.1.py
@@ -11,11 +11,12 @@ def solution(list):
   for i in range(1,listsize):
     if min > list[i]:  #설정한 min 값이 배열의 값보다 크면  
       min=list[i]      #배열의 값을 min으로 설정
+      min_indx=i
 
-  print("minimun value is ",min)
+  print("minimun index value is ",min_indx)
 
 
-print(solution([7,10,5,2,15,19]))
+print(solution([7,10,5,3,2,15,19]))
 print(solution([-12,12,30,-15,-5,3,9,-11,14]))
 print(solution([17,11,5,8,23,29,19,12,25,16,-2]))
 print(solution([7,5,12,-9,-12,22,-30,-35,-21]))
