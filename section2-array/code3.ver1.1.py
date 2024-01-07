600c8d65f2537046d101eb6e649df79dacfa0237
jihye-song1 <rabbit3919@gmail.com>
2024-01-07 20:01:16 +0900

    SECTION2.배열과 연결리스트_문제3_v1.1

--- section2-array/code3.ver1.1.py
+++ section2-array/code3.ver1.1.py
@@ -2,7 +2,7 @@
 # 1이 연속된 부분수열 중 가장 긴 부분수열의 길이를 반환하는 프로그램을 작성
 
 def solution(nums):
-    listsize=len(nums)      # 배열 길이 받아오기
+    listsize=len(nums)      # 배열 길이 받아옴
     answer=0                # 정답 변수 ( 부분수열 중 1이 없을수도 있기 때문에 0으로 초기화 )
     count=0                 # count 변수 
 
@@ -15,7 +15,7 @@ def solution(nums):
             count=0                     # 0을 만났으니 count값을 0으로 다시 초기화 
     answer = max(answer,count)
     print("answer is ",answer)
-    
+
 
 print(solution([1,1,0,1,1,1,0,1,1,1,1,1]))
 print(solution([0,0,1,0,1,0,0]))
